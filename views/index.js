da76015ecbd7a8f478a79f82cbccee8b47078e3a
sylvainf <ferrand.sylvain@gmail.com>
2014-07-16 14:09:31 +0200

    Premiers test issue #3

--- views/index.js
+++ views/index.js
@@ -1,5 +1,31 @@
+
 'use strict';
 
-exports.init = function(req, res){
-  res.render('index');
+exports.init = function(req, res, next){
+  var sigma = {};
+  var collections = ['User', 'Account', 'Game'];
+  var queries = [];
+
+  collections.forEach(function(el, i, arr) {
+    queries.push(function(done) {
+      req.app.db.models[el].count({}, function(err, count) {
+        if (err) {
+          return done(err, null);
+        }
+
+        sigma['count'+ el] = count;
+        done(null, el);
+      });
+    });
+  });
+
+  var asyncFinally = function(err, results) {
+    if (err) {
+      return next(err);
+    }
+
+    res.render('index', sigma);
+  };
+
+  require('async').parallel(queries, asyncFinally);
 };
