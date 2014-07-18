ef279d3fac896b64b1ccac408d23bee88b179dee
sylvainf <ferrand.sylvain@gmail.com>
2014-07-18 17:22:21 +0200

    Issue #14

--- views/account/index.js
+++ views/account/index.js
@@ -1,5 +1,10 @@
 'use strict';
 
 exports.init = function(req, res){
-  res.render('account/index');
+	console.log(req.user.username);
+	req.app.db.models.User.findOne({ username: req.user.username }, 'username email',  function (err, user) {
+		req.app.db.models.Account.findById(req.user.roles.account.id, 'name',  function (err, account) {
+			res.render('account/index', {email: user.email, account: account})
+      });
+	});
 };
