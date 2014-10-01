233784089eb2ead71aa300e3d07ad3109c44c495
Eric Ramat <eric.ramat@gmail.com>
2014-10-01 22:16:59 +0200

    Fix view/index.js

--- views/index.js
+++ views/index.js
@@ -18,45 +18,45 @@ exports.init = function (req, res, next) {
         });
     });
 
-	 queries.push(function (done) {
-		 req.app.db.models.GameHisto.find({ }, null,
-			 { safe: true }, function (err, gamehisto) {
-				 if (gamehisto!="") { 
-					 console.log('gamehisto');   
-					 for (var key in gamehisto) {
-						 var itemdata = {};
-						 var item = gamehisto[key];
+    queries.push(function (done) {
+        req.app.db.models.GameHisto.find({ }, null,
+            { safe: true }, function (err, gamehisto) {
+                if (gamehisto != "") {
+                    for (var key in gamehisto) {
+                        var itemdata = {};
+                        var item = gamehisto[key];
 
-						 req.app.db.models.User.findOne({_id: item.userCreated.id }, null,
-							 { safe: true }, function (err, userCreated) {
-								 itemdata.userCreated = userCreated.username;
-								 req.app.db.models.User.findOne({_id: item.opponent.id }, null,
-									 { safe: true }, function (err, opponent) {
-										 itemdata.opponent = opponent.username
-										 req.app.db.models.User.findOne({_id: item.winner.id }, null,
-											 { safe: true }, function (err, winner) {
-												 itemdata.winner = winner.username
-												 req.app.db.models.GameType.findOne({_id: item.game }, null,
-													 { safe: true }, function (err, gametype) {
-														 itemdata.gameType = gametype.name
-														 last.push(itemdata);
-														 done(null);									  
-													 });
-												 });
-											 });
-										 });
-									 }
-								 } else { 
-									 done(null);
-								 }
-							 });
-						 });
+                        req.app.db.models.User.findOne({_id: item.userCreated.id }, null,
+                            { safe: true }, function (err, userCreated) {
+                                itemdata.userCreated = userCreated.username;
+                                req.app.db.models.User.findOne({_id: item.opponent.id }, null,
+                                    { safe: true }, function (err, opponent) {
+                                        itemdata.opponent = opponent.username
+                                        req.app.db.models.User.findOne({_id: item.winner.id }, null,
+                                            { safe: true }, function (err, winner) {
+                                                itemdata.winner = winner.username
+                                                req.app.db.models.GameType.findOne({_id: item.game }, null,
+                                                    { safe: true }, function (err, gametype) {
+                                                        itemdata.gameType = gametype.name
+                                                        last.push(itemdata);
+                                                        if (last.length === gamehisto.length) {
+                                                            done(null, last);
+                                                        }
+                                                    });
+                                            });
+                                    });
+                            });
+                    }
+                } else {
+                    done(null);
+                }
+            });
+    });
 
     var asyncFinally = function (err, results) {
         if (err) {
             return next(err);
         }
-		  console.log(last);
         res.render('index', { count: sigma, histo: last });
     };
 
